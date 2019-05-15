526cb31fb353054f24772c39a93b9b5da6a390d7
Your Name <1595320647@qq.com>
2019-05-15 19:57:24 +0800

    第一次架构提交

---
+++ src/main/java/com/qf/Main.java
@@ -0,0 +1,14 @@
+package com.qf;
+
+import java.sql.SQLOutput;
+
+/**
+ * @version 1.0
+ * @user Administrator
+ * @date 2019/5/15 19:21
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
